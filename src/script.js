13b138965257586cf154e28e371dc6a3edf44b8a
natacha <nat_bartu@hotmail.com>
2022-09-27 15:07:50 +1000

    commit

--- src/script.js
+++ src/script.js
@@ -12,17 +12,19 @@ function resetDOM() {
   nums.forEach((num) => {
     num.classList.value = "";
   });
+
   nums[0].classList.add("in");
 }
 
 function runAnimation() {
   nums.forEach((num, idx) => {
     const nextToLast = nums.length - 1;
+
     num.addEventListener("animationend", (e) => {
       if (e.animationName === "goIn" && idx !== nextToLast) {
         num.classList.remove("in");
         num.classList.add("out");
-      } else if (e - animationName === "goOut" && num.nextElementSibling) {
+      } else if (e.animationName === "goOut" && num.nextElementSibling) {
         num.nextElementSibling.classList.add("in");
       } else {
         counter.classList.add("hide");
